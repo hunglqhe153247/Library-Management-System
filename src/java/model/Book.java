2fd91f0ed76e845b3f745743a0b9f1dcf820b5fe
hunglqhe153247 <92884206+hunglqhe153247@users.noreply.github.com>
2022-06-19 13:51:46 +0700

    update

---
+++ src/java/model/Book.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Hung
+ */
+public class Book {
+    String id;
+    String name;
+    String author;
+    String year;
+
+    public Book() {
+    }
+
+    public Book(String id, String name, String author, String year) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+    
+}
